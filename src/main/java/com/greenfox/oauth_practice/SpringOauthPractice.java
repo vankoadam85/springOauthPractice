1796399d80a6ac3c908b28023962d5d1eb67d1bb
Vankó <vankoadam85@gmail.com>
2019-03-25 16:59:53 +0100

    Add JWT generation and redirect configurations

--- src/main/java/com/greenfox/oauth_practice/SpringOauthPractice.java
+++ src/main/java/com/greenfox/oauth_practice/SpringOauthPractice.java
@@ -1,9 +1,12 @@
 package com.greenfox.oauth_practice;
 
+import com.greenfox.oauth_practice.configuration.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class SpringOauthPractice {
 
   public static void main(String[] args) {
