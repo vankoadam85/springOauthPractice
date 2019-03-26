1e939f590e8efc7b89a119b159075716449ba08e
Vankó <vankoadam85@gmail.com>
2019-03-26 17:43:31 +0100

    Add SecurityConfig. Add getters/setters to User model

--- src/main/java/com/greenfox/oauth_practice/entity/User.java
+++ src/main/java/com/greenfox/oauth_practice/entity/User.java
@@ -29,4 +29,51 @@ public class User {
 
   private String providerId;
 
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public AuthProvider getProvider() {
+    return provider;
+  }
+
+  public void setProvider(AuthProvider provider) {
+    this.provider = provider;
+  }
+
+  public String getProviderId() {
+    return providerId;
+  }
+
+  public void setProviderId(String providerId) {
+    this.providerId = providerId;
+  }
 }
--- src/main/java/com/greenfox/oauth_practice/repository/UserRepository.java
+++ src/main/java/com/greenfox/oauth_practice/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.greenfox.oauth_practice.repository;
 
 import com.greenfox.oauth_practice.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
   Optional<User> findByEmail(String email);
